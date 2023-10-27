d5dfaa5b9c6fa1ecdc43e5094dd6449253d79083
Shamimreza82 <shamimrezaone@gmail.com>
2023-10-27 20:33:22 +0600

    added jwt token in for user verify

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000; 
 
 //middelwair
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true
+}))
 app.use(express.json())
-app.use(cors())
-
-
-// shamimrezabd67
-// 2qzkSvRbQgULjSI0
-console.log(process.env.DB_USER);
-console.log(process.env.DB_PASS);
+app.use(cookieParser())
 
 
 
@@ -30,10 +30,130 @@ const client = new MongoClient(uri, {
   }
 });
 
+  const verifyToken = async (req, res, next) => {
+    const token = req.cookies?.token
+    console.log('value of token in middielware', token);
+    if(!token){
+      return res.status(401).send({message: "not authorize"})
+    }
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if(err) {
+        console.log(err);
+        return res.status(401).send({message: "Unauthorouz"}) 
+      }
+      console.log("value in the", decoded );
+      req.user = decoded; 
+      next()
+    })
+    
+  }
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const serviceCollection = client.db('carDoctor').collection('services')
+    const checkoutCollection = client.db('carDoctor').collection('checkout')
+
+
+
+    app.post('/jwt', async (req, res) => {
+      const user = req.body
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+      res
+      .cookie('token', token, {
+        httpOnly: true, 
+        secure: false, 
+      })
+      .send({success: true})
+    })
+
+
+
+
+    /// services related api
+    app.get('/services', async (req, res) => {
+        const cursor = serviceCollection.find(); 
+        const result = await cursor.toArray()
+        res.send(result)
+    })
+
+    app.get ('/services/:id', async (req, res) => {
+        const id = req.params.id; 
+        const quary = {_id: new ObjectId (id)};
+
+        const options = {
+            projection: { title: 1, price: 1, img: 1, service_id: 1  },
+          };
+
+        const result = await serviceCollection.findOne(quary, options); 
+        res.send(result)
+    } )
+
+    /// bookinges
+
+    app.get('/checkout', verifyToken, async (req, res) => {
+        console.log(req.query.email);
+        // console.log("token", req.cookies.token);
+        console.log('from valid token', req.user);
+        if(req.query.email !== req.user.email){
+            return res.status(403).send({message: 'forbidden'})
+        }
+        let query = {}
+        if(req.query?.email){
+            query = {email: req.query.email}
+        }
+        const result = await checkoutCollection.find(query).toArray()
+        res.send(result)
+    })
+
+    app.post('/checkout', async (req, res) => {
+        const bookings = req.body; 
+        console.log(bookings);
+        const result = await checkoutCollection.insertOne(bookings)
+        res.send(result)
+    })
+
+    app.patch('/checkout/:id', async (req, res) => {
+      const updateed = req.body; 
+      console.log(updateed);
+      const id = req.params.id; 
+      const filter = {_id: new ObjectId(id)}; 
+      const updateDoc = {
+        $set: {
+          status: updateed.status
+        },
+      };
+      const result = await checkoutCollection.updateOne(filter, updateDoc)
+      res.send(result)
+
+    })
+   
+
+
+    app.delete('/checkout/:id',async (req, res) => {
+        const id = req.params.id
+        const queary = {_id: new ObjectId(id)}
+        const result= await checkoutCollection.deleteOne(queary)
+        res.send(result)
+    })
+
+
+    // Product Detailes route
+
+    app.get('/servicesDetails/:id', async (req, res) => {
+        const id = req.params.id; 
+        const query = {_id: new ObjectId(id)}
+        const result = await serviceCollection.findOne(query)
+        res.send(result)
+    })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
